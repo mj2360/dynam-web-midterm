d490971fb565d71e87586b5b01c3c2f9614425a2
mj2360 <mj2360@nyu.edu>
2021-10-18 10:37:28 -0400

    created word input

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,11 +1,16 @@
-import React from 'react'; 
+import React, { useEffect,useMemo, useState } from 'react'; 
+import axios from 'axios'; 
 
 function Home() {
+    let initialWord = "hello"
+    const [word, setWord] = useState(initialWord);
     return (
-        <div>
-            <p> Something Something</p>
-        </div>
-    )
+      <>
+        Word: {word}
+        <input type="text" placeholder="Type Word" id="input"></input>
+        <button onClick={() => setWord(document.getElementById("input").value)}>Submit</button>
+      </>
+    );
 }
 
 export default Home; 
