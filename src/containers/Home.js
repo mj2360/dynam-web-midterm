265c625fa4efb4a8829d4607bd9ec265454401b4
mj2360 <mj2360@nyu.edu>
2021-10-18 10:47:36 -0400

    console logging json data from api

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,9 +1,24 @@
-import React, { useEffect,useMemo, useState } from 'react'; 
+import React, { useEffect, useMemo, useState } from 'react'; 
 import axios from 'axios'; 
 
 function Home() {
     let initialWord = "hello"
     const [word, setWord] = useState(initialWord);
+    const[wordData, setWordData] = useState(); 
+    useEffect(() => {
+        if(word) {
+           axios.get(
+               `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`
+           )
+           .then(function (response) {
+             setWordData(response.data);
+             console.log(wordData)
+           })
+           .catch(function (error) {
+             console.warn(error);
+           });
+        }
+       }, [word]); 
     return (
       <>
         Word: {word}
@@ -11,6 +26,8 @@ function Home() {
         <button onClick={() => setWord(document.getElementById("input").value)}>Submit</button>
       </>
     );
+
+   
 }
 
 export default Home; 
